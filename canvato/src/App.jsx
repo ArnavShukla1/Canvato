955b93f983763ad807628a8c9799ac7641d35834
Arnav Shukla <arnavshukla@Arnavs-MacBook-Pro.local>
2024-10-15 09:13:25 -0400

    set up tailwindcss

---
+++ canvato/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p className='text-5xl text-center'>Hello World</p>
+      </div>
+    </>
+  )
+}
+
+export default App
